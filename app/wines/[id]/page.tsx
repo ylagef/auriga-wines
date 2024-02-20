d8194bab467af9a45cdc9e288072802cf18a291f
Yeray Lage <ylagef@gmail.com>
2024-02-20 18:25:28 +0100

    feat: add detail data

--- app/wines/[id]/page.tsx
+++ app/wines/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/Badge";
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 
@@ -17,9 +18,14 @@ async function WineDetail({
 
   if (!wine) return <div>Wine not found</div>;
 
+  const { data: grapesData, error: errorGrapes } = await supabase
+    .from("grapes")
+    .select("id, name")
+    .in("id", wine.grapes);
+
   const size = wine.photo_size as { width: number; height: number };
   return (
-    <div className="flex flex-col items-center w-full gap-8 animate-fade-in">
+    <div className="flex flex-col items-center w-full gap-4 animate-fade-in">
       <Image
         src={`https://jacopngdwpoypvunhunq.supabase.co/storage/v1/object/public/wines/${wine.photo_url}`}
         alt={wine.name}
@@ -27,6 +33,22 @@ async function WineDetail({
         height={size.height || 100}
         className="object-contain w-3/4 aspect-square"
       />
+
+      <div className="flex gap-2">
+        {wine.new && <Badge variant="default">Nuevo</Badge>}
+        <Badge variant="secondary">{wine.year}</Badge>
+      </div>
+
+      <div className="flex flex-wrap items-center justify-center gap-1">
+        {grapesData
+          ?.filter((grape) => wine.grapes.includes(grape.id))
+          .map((grape) => (
+            <Badge key={grape.id} variant="outline">
+              {grape.name}
+            </Badge>
+          ))}
+      </div>
+
       <h3 className="font-bold text-center">{wine.name}</h3>
 
       <p className="text-sm text-center text-gray-600">{wine.description}</p>
