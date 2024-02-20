1eaaf34c87385364541fd1238911aad5c324cfce
Yeray Lage <ylagef@gmail.com>
2024-02-20 18:37:37 +0100

    feat: center footer

--- app/wines/layout.tsx
+++ app/wines/layout.tsx
@@ -7,7 +7,7 @@ function WinesLayout({ children }: { children: ReactNode }) {
         <main className="py-2">{children}</main>
       </div>
       <footer className="flex items-center justify-center w-full p-2">
-        <p className="text-xs opacity-50">
+        <p className="text-xs text-center opacity-50">
           Todas nuestras botellas pueden ser compradas para consumir fuera del
           local a un menor precio.
         </p>
