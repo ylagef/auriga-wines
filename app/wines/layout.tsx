9c7bb133a1cfccf09e6ca05191325b8f64216c0c
Yeray Lage <ylagef@gmail.com>
2024-02-20 17:51:55 +0100

    feat: add footer

--- app/wines/layout.tsx
+++ app/wines/layout.tsx
@@ -2,9 +2,17 @@ import { ReactNode } from "react";
 
 function WinesLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full max-w-4xl p-2">
-      <main>{children}</main>
-    </div>
+    <>
+      <div className="w-full max-w-4xl p-2 grow">
+        <main className="py-2">{children}</main>
+      </div>
+      <footer className="flex items-center justify-center w-full p-2">
+        <p className="text-xs opacity-50">
+          Todas nuestras botellas pueden ser compradas para consumir fuera del
+          local a un menor precio.
+        </p>
+      </footer>
+    </>
   );
 }
 
